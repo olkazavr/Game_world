ddad601d7ac373af24e93e4f3e3427c5ef1c6cfd
olkazavr <123472307+olkazavr@users.noreply.github.com>
2024-05-01 22:21:59 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const searchInput = document.querySelector("#search");
-const divs = document.querySelectorAll(".movie");
+const divs = document.querySelectorAll(".game");
 
 searchInput.addEventListener("keyup", function(event){
     const word = event.target.value.toLowerCase();
@@ -8,3 +8,19 @@ searchInput.addEventListener("keyup", function(event){
         item.querySelector("p").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none")
     })
 })
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let imageContainers = document.querySelectorAll('.image-container');
+    const overlayButtons = document.querySelectorAll('.overlay-button');
+
+    imageContainers.forEach(function(item, index) {
+        item.onmouseover = function() {
+        overlayButtons[index].style.display = 'block';
+    };
+
+        item.onmouseout = function() {
+        overlayButtons[index].style.display = 'none';
+    };
+    });
+});
